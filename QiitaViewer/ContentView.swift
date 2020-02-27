5a9f5b758941b3843f1ac42f996f12ec9ebe58a8
asashin227 <asa.shin.asa@gmail.com>
2020-02-27 11:58:51 +0900

    Initial Commit

---
+++ QiitaViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QiitaViewer
+//
+//  Created by Asakura  Shinsuke on 2020/02/27.
+//  Copyright © 2020 Asakura  Shinsuke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
